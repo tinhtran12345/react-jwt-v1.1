20993dac1838bf19bd136c117439a02ece26a53a
tinhtran12345 <tinhtran04012002@gmail.com>
2023-05-04 11:47:21 -0500

    setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+
+import HomePage from "./components/Home/HomePage";
+
+function App() {
+    // const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <HomePage />
+        </>
+    );
+}
+
+export default App;
